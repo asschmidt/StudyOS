71d00dc4dc69529b55b777a42246c16f4138ed5f
Andreas Schmidt <a.v.schmidt81@googlemail.com>
2025-08-01 22:01:14 +0200

    Added info outputs to generate_floppy script

--- tools/scripts/generate_floppy.sh
+++ tools/scripts/generate_floppy.sh
@@ -13,9 +13,14 @@
 # Sector 1: Bootsector
 # Sector 257: Start of File-System Partition
 
+echo "Generating Image-File with 1,44 MiB"
 dd if=/dev/zero of=$1 bs=512 count=2880 >/dev/null
+
+echo "Creating Partition Table"
 parted -s $1 mktable msdos
+echo "Creating Bootloader Stage 2 Partition"
 parted -s $1 -- mkpart primary fat16 1s 256s
+echo "Creating Kernel & FAT12 Filesystem Partition"
 parted -s $1 -- mkpart primary fat16 257s -1s
 #parted -s $1 set on boot 0
 fdisk -lu $1
