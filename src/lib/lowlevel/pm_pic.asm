9d35394ec62c3283afcfa668d7de40fedd60b392
Andreas Schmidt <a.v.schmidt81@googlemail.com>
2025-08-31 14:03:41 +0200

    Changed PIC file to use preprocessor defines instead of .set

--- src/lib/lowlevel/pm_pic.asm
+++ src/lib/lowlevel/pm_pic.asm
@@ -6,36 +6,54 @@
 
 .intel_syntax noprefix
 
+/* IO Port Address for PIC1 */
+#define PIC1_IO_ADDR       0x20
+/* IO Port Address for PIC2 */
+#define PIC2_IO_ADDR       0xA0
 
-.set PIC1_IO_ADDR,      0x20            /* IO Port Address for PIC1 */
-.set PIC2_IO_ADDR,      0xA0            /* IO Port Address for PIC2 */
+/* IO Port Address for PIC1 Command Register */
+#define PIC1_CMD_REG       PIC1_IO_ADDR
+/* IO Port Address for PIC1 Data Register */
+#define PIC1_DATA_REG      PIC1_IO_ADDR + 1
 
-.set PIC1_CMD_REG, PIC1_IO_ADDR         /* IO Port Address for PIC1 Command Register */
-.set PIC1_DATA_REG, PIC1_IO_ADDR + 1    /* IO Port Address for PIC1 Data Register */
-
-.set PIC2_CMD_REG, PIC2_IO_ADDR         /* IO Port Address for PIC2 Command Register */
-.set PIC2_DATA_REG, PIC2_IO_ADDR + 1    /* IO Port Address for PIC2 Data Register */
+/* IO Port Address for PIC2 Command Register */
+#define PIC2_CMD_REG       PIC2_IO_ADDR
+/* IO Port Address for PIC2 Data Register */
+#define PIC2_DATA_REG      PIC2_IO_ADDR + 1
 
 
 /* PIC Commands */
-.set PIC_EOI,           0x20            /* End of Interrupt Command */
-.set PIC_READ_IRR,      0x0a            /* OCW3 irq ready next CMD read */
-.set PIC_READ_ISR,      0x0b            /* OCW3 irq service next CMD read */
+/* End of Interrupt Command */
+#define PIC_EOI            0x20
+/* OCW3 irq ready next CMD read */
+#define PIC_READ_IRR       0x0a
+/* OCW3 irq service next CMD read */
+#define PIC_READ_ISR       0x0b
 
 /* PIC onfigure Bit Values */
-.set ICW1_ICW4,	        0x01		    /* Indicates that ICW4 will be present */
-.set ICW1_SINGLE,	    0x02		    /* Single (cascade) mode */
-.set ICW1_INTERVAL4,	0x04		    /* Call address interval 4 (8) */
-.set ICW1_LEVEL,	    0x08		    /* Level triggered (edge) mode */
-.set ICW1_INIT,	        0x10		    /* Initialization - required! */
+/* Indicates that ICW4 will be present */
+#define ICW1_ICW4 	        0x01
+/* Single (cascade) mode */
+#define ICW1_SINGLE 	    0x02
+/* Call address interval 4 (8) */
+#define ICW1_INTERVAL4  	0x04
+/* Level triggered (edge) mode */
+#define ICW1_LEVEL  	    0x08
+/* Initialization - required! */
+#define ICW1_INIT           0x10
 
-.set ICW4_8086,	        0x01		    /* 8086/88 (MCS-80/85) mode */
-.set ICW4_AUTO,	        0x02		    /* Auto (normal) EOI */
-.set ICW4_BUF_SLAVE,	0x08		    /* Buffered mode/slave */
-.set ICW4_BUF_MASTER,	0x0C		    /* Buffered mode/master */
-.set ICW4_SFNM,	        0x10		    /* Special fully nested (not) */
+/* 8086/88 (MCS-80/85) mode */
+#define ICW4_8086 	        0x01
+/* Auto (normal) EOI */
+#define ICW4_AUTO 	        0x02
+/* Buffered mode/slave */
+#define ICW4_BUF_SLAVE  	0x08
+/* Buffered mode/master */
+#define ICW4_BUF_MASTER 	0x0C
+/* Special fully nested (not) */
+#define ICW4_SFNM           0x10
 
-.set CASCADE_IRQ,       2
+#define CASCADE_IRQ         2
 
 
 /*
@@ -64,14 +82,14 @@ pmPICSendEOI:
     jl .handlePIC1_pmPICSendEOI
 
 .handlePIC2_pmPICSendEOI:
-    push PIC_EOI
-    push PIC2_CMD_REG
+    push PIC_EOI                /* PIC_EOI */
+    push PIC2_CMD_REG           /* PIC2_CMD_REG */
     call pmPortOutByte
     add esp, 8
 
 .handlePIC1_pmPICSendEOI:
-    push PIC_EOI
-    push PIC1_CMD_REG
+    push PIC_EOI                /* PIC_EOI */
+    push PIC1_CMD_REG           /* PIC1_CMD_REG */
     call pmPortOutByte
     add esp, 8
 
@@ -108,70 +126,70 @@ pmPICRemap:
     mov esi, [ebp + 12]
 
     /* Start Initialization in Cascade Mode */
-    push ICW1_INIT | ICW1_ICW4
-    push PIC1_CMD_REG
+    push ICW1_INIT | ICW1_ICW4              /* ICW1_INIT | ICW1_ICW4 */
+    push PIC1_CMD_REG                       /* PIC1_CMD_REG */
     call pmPortOutByte
     add esp, 8
     call pmPortWait
 
-    push ICW1_INIT | ICW1_ICW4
-    push PIC2_CMD_REG
+    push ICW1_INIT | ICW1_ICW4              /* ICW1_INIT | ICW1_ICW4 */
+    push PIC2_CMD_REG                       /* PIC2_CMD_REG */
     call pmPortOutByte
     add esp, 8
     call pmPortWait
 
     /* Set Offset for Master PIC (PIC1) */
     push edi
-    push PIC1_DATA_REG
+    push PIC1_DATA_REG                      /* PIC1_DATA_REG */
     call pmPortOutByte
     add esp, 8
     call pmPortWait
 
     /* Set Offset for Slave PIC (PIC2) */
     push esi
-    push PIC2_DATA_REG
+    push PIC2_DATA_REG                      /* PIC2_DATA_REG */
     call pmPortOutByte
     add esp, 8
     call pmPortWait
 
     /* Tell Master PIC about the cascade IRQ 2 */
-    push 1 << CASCADE_IRQ
-    push PIC1_DATA_REG
+    push 1 << CASCADE_IRQ                   /* 1 << CASCADE_IRQ */
+    push PIC1_DATA_REG                      /* PIC1_DATA_REG */
     call pmPortOutByte
     add esp, 8
     call pmPortWait
 
     /* Tell Slave PIC about cascade identity 0000 0010 */
-    push CASCADE_IRQ
-    push PIC2_DATA_REG
+    push CASCADE_IRQ                        /* CASCADE_IRQ */
+    push PIC2_DATA_REG                      /* PIC2_DATA_REG */
     call pmPortOutByte
     add esp, 8
     call pmPortWait
 
     /* Tell Master PIC to use 8086 Mode */
-    push ICW4_8086
-    push PIC1_DATA_REG
+    push ICW4_8086                          /* ICW4_8086 */
+    push PIC1_DATA_REG                      /* PIC1_DATA_REG */
     call pmPortOutByte
     add esp, 8
     call pmPortWait
 
     /* Tell Slave PIC to use 8086 Mode */
-    push ICW4_8086
-    push PIC2_DATA_REG
+    push ICW4_8086                          /* ICW4_8086 */
+    push PIC2_DATA_REG                      /* PIC2_DATA_REG */
     call pmPortOutByte
     add esp, 8
     call pmPortWait
 
     /* Unmask Master PIC */
     push 0x00
-    push PIC1_DATA_REG
+    push PIC1_DATA_REG                      /* PIC1_DATA_REG */
     call pmPortOutByte
     add esp, 8
     call pmPortWait
 
     /* Unmask Slave PIC */
     push 0x00
-    push PIC2_DATA_REG
+    push PIC2_DATA_REG                      /* PIC2_DATA_REG */
     call pmPortOutByte
     add esp, 8
     call pmPortWait
@@ -209,13 +227,13 @@ pmPICReadIRQReg:
 
     /* Send OCW3 value to PIC1 */
     push edi
-    push PIC1_CMD_REG
+    push PIC1_CMD_REG                       /* PIC1_CMD_REG */
     call pmPortOutByte
     add esp, 8
 
     /* Send OCW3 value to PIC2 */
     push edi
-    push PIC2_CMD_REG
+    push PIC2_CMD_REG                       /* PIC2_CMD_REG */
     call pmPortOutByte
     add esp, 8
 
@@ -223,7 +241,7 @@ pmPICReadIRQReg:
     xor eax, eax
 
     /* Read command register from PIC1 containing the requested register value */
-    push PIC1_CMD_REG
+    push PIC1_CMD_REG                       /* PIC1_CMD_REG */
     call pmPortInByte
     add esp, 4
 
@@ -234,7 +252,7 @@ pmPICReadIRQReg:
     xor eax, eax
 
     /* Read command register from PIC2containing the requested register value */
-    push PIC2_CMD_REG
+    push PIC2_CMD_REG                       /* PIC2_CMD_REG */
     call pmPortInByte
     add esp, 4
 
@@ -268,7 +286,7 @@ pmPICReadIRR:
     push ebp
     mov ebp, esp
 
-    push PIC_READ_IRR
+    push PIC_READ_IRR                       /* PIC_READ_IRR */
     call pmPICReadIRQReg
     add esp, 4
 
@@ -295,7 +313,7 @@ pmPICReadISR:
     push ebp
     mov ebp, esp
 
-    push PIC_READ_ISR
+    push PIC_READ_ISR                       /* PIC_READ_ISR */
     call pmPICReadIRQReg
     add esp, 4
 
@@ -332,12 +350,12 @@ pmPICSetMask:
     /* If the IRQ Line is less than 8, we only mask on PIC1 */
     jl .maskPIC1_pmPICSetMask
     /* Otherwise we mask on PIC2 by using the PIC2 Data Register and correcting the IRQ Line for PIC2 */
-    mov esi, PIC2_DATA_REG
+    mov esi, PIC2_DATA_REG                  /* PIC2_DATA_REG */
     sub edi, 8
     jmp .maskWrite_pmPICSetMask
 
 .maskPIC1_pmPICSetMask:
-    mov esi, PIC1_DATA_REG
+    mov esi, PIC1_DATA_REG                  /* PIC1_DATA_REG */
 
 .maskWrite_pmPICSetMask:
 
@@ -388,12 +406,12 @@ pmPICDisable:
     mov ebp, esp
 
     push 0xFF
-    push PIC1_DATA_REG
+    push PIC1_DATA_REG                      /* PIC1_DATA_REG */
     call pmPortOutByte
     add esp, 8
 
     push 0xFF
-    push PIC2_DATA_REG
+    push PIC2_DATA_REG                      /* PIC2_DATA_REG */
     call pmPortOutByte
     add esp, 8
 
